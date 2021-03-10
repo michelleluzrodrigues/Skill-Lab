c02e748e690675e9dc7bdf8cedb32df8b4def881
michelleluzrodrigues <michellerodrigues268@gmail.com>
2021-03-10 20:34:33 -0300

    SkillLab

--- Desafio_Aula_6/Exercicio_10/js/script.js
+++ Desafio_Aula_6/Exercicio_10/js/script.js
@@ -6,15 +6,18 @@ function fMasc(objeto, mascara) {
     setTimeout("fMascEx()", 1)
 }
 function fMascEx() {
-    obj.value = masc(obj.value)
+    obj.value = mCPF(obj.value)
 }
 
 function mCPF(cpf) {
-    var cpf1 = document.getElementById('#cpf1');
+    var cpf1 = document.getElementById('cpf1');
     cpf = cpf.replace(/\D/g, "")
     cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
     cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
     cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    cpf1.value = cpf.replace(/[.-]/g, "");
+
+
     return cpf;
 }
 
