670c9af6bcd1d67474b8e436fb319aa81ab5d3f6
michelleluzrodrigues <michellerodrigues268@gmail.com>
2021-03-10 21:14:54 -0300

    Atualização exercicios

---
+++ Desafio_Aula_6/Exercicio_11/js/script.js
@@ -0,0 +1,7 @@
+function somar(num1, num2) {
+    var num1 = parseInt(document.getElementById('num1').value);
+    var num2 = parseInt(document.getElementById('num2').value);
+    document.getElementById('resultado').value = num1 + num2;
+
+
+}
