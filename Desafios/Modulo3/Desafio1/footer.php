cda5bd4e7591516c296f71e7d9b3b611043dfbf6
michelleluzrodrigues <michellerodrigues268@gmail.com>
2021-03-31 17:09:00 -0300

    SkillLab

---
+++ Desafios/Modulo3/Desafio1/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="footer">
+        &copy; Copyright <?php auto_copyright("2020"); ?>
+    </div>
+</footer>
+
+
+<?php function auto_copyright($year = 'auto'){ ?>
+   <?php if(intval($year) == 'auto'){ $year = date('Y'); } ?>
+   <?php if(intval($year) == date('Y')){ echo intval($year); } ?>
+   <?php if(intval($year) < date('Y')){ echo intval($year) . ' - ' . date('Y'); } ?>
+   <?php if(intval($year) > date('Y')){ echo date('Y'); } ?>
+<?php } ?>
---
+++ Desafios/Modulo3/Desafio1/ola.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+           <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>Desafio 1</title>
+    </head>
+    <body>
+       <div class="container">
+  <div class="row">
+    <div class="col-sm">
+        <h1>titulo 1</h1>
+        <h2>Titulo 2</h2>
+        <p>coluna 1</p>
+    </div>
+    <div class="col-sm">
+     <h1>titulo 1</h1>
+        <h2>Titulo 2</h2>
+        <p>coluna 2</p>
+    </div>
+    <div class="col-sm">
+      <h1>titulo 1</h1>
+        <h2>Titulo 2</h2>
+        <p>coluna 3</p>
+    </div>
+  </div>
+</div>
+        <?php include 'footer.php'; ?>
+        
+    </body>
+</html>
